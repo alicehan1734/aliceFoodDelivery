640ce124c197dd69f2f76220dab8fd05e13370d5
heeyeonhan <heeyeonh3@gmail.com>
2021-10-13 14:27:31 -0400

    Using express express-handlebars

--- assignment1/server.js
+++ assignment1/server.js
@@ -12,25 +12,98 @@
 
 var HTTP_PORT = process.env.PORT || 8080;
 var path = require("path");
-const mymodule = require("./modules/mymodules.js");
-
 var express = require("express");
-const exphbs = require('express-handlerbars');
-
+const exphbs = require('express-handlebars');
+var path = require("path");
 
 var app = express();
 
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: "main"
+}));
 
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-
 var path = require("path");
 
 // setup a 'route' to listen on the default url path
 app.use(express.static('static'));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/home.html"))
+
+  var topMeals = [
+    {
+      title: "Homemade Supreme Pizza",
+      included: "Pizza sauce, mozzarella, bacon, onion, beef mince, capsicum, pepperoni, mushroom, olives",
+      desc: "very delicious",
+      category: "Classic Meals",
+      price: 30.00,
+      time: 25.78,
+      serv: 2,
+      calperServ: 890,
+      img: "./images/ingredient1.jpg",
+      top: true,
+      type: "breakfast"
+    },
+    {
+      title: "Mad Radish",
+      included: "Eggs, green beans, Kalamata olives, tomatoes, tuna, and tangy potatoes",
+      desc: "very delicious",
+      category: "Classic Meals",
+      price: 28.03,
+      time: 25.78,
+      serv: 2,
+      calperServ: 890,
+      img: "./images/ingredient2.jpg",
+      top: true,
+      type: "breakfast"
+    },
+    {
+      title: "Pasta Pronta",
+      included: "Egg for colour and richness (in some types of pasta), and possibly vegetable juice (such as spinach, beet, tomato, carrot), herbs",
+      desc: "very delicious",
+      category: "Classic Meals",
+      price: 15.22,
+      time: 25.78,
+      serv: 2,
+      calperServ: 890,
+      img: "./images/ingredient3.jpg",
+      top: true,
+      type: "breakfast"
+    },
+    {
+      title: "Tteok-bokki",
+      included: "Tteok (rice cakes), fishcake, gochujang",
+      desc: "very delicious",
+      category: "Classic Meals",
+      price: 22.23,
+      time: 25.78,
+      serv: 2,
+      calperServ: 890,
+      img: "./images/ingredient4.jpg",
+      top: true,
+      type: "breakfast"
+    },
+    {
+      title: "Yonge Burger",
+      included: "Ground beef, egg, beaten, dry bread crumbs, evaporated milk, Worcestershire sauce, cayenne pepper, garlic, minced",
+      desc: "very delicious",
+      category: "Classic Meals",
+      price: 55.2,
+      time: 25.78,
+      serv: 2,
+      calperServ: 890,
+      img: "./images/ingredient5.jpg",
+      top: true,
+      type: "breakfast"
+    }
+  ];
+
+  res.render("home", {
+    topMeals,
+    layout: false // do not use the default layout
+  });
+
 });
 
 app.get("/menu", (req, res) => {
