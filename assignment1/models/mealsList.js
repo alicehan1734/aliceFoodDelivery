83c6e0d4b573a477d7b34115c12d132f8c30c8da
heeyeonhan <heeyeonh3@gmail.com>
2021-10-18 18:32:46 -0400

    no message

--- assignment1/models/mealsList.js
+++ assignment1/models/mealsList.js
@@ -35,7 +35,7 @@ var meals = [
     serv: 2,
     calperServ: 1000,
     img: "./images/ingredient3.jpg",
-    top: true,
+    top: false,
     type: "breakfast"
   },
   {
@@ -65,72 +65,192 @@ var meals = [
     type: "breakfast"
   },
   {
-    title: "Pizza",
-    included: "Pizza sauce, mozzarella, bacon, onion, beef mince, capsicum, pepperoni, mushroom, olives",
+    title: "Pecan chocolate bread and butter pudding",
+    included: "Bread, eggs, sugar, chocolate buttons, milk. ",
     desc: "very delicious",
-    category: "Classic Meals",
+    category: "Easy Prep Meals",
+    price: 12.15,
+    time: 34,
+    serv: 1,
+    calperServ: 90,
+    img: "./images/dessert1.jpg",
+    top: false,
+    type: "breakfast"
+  },
+  {
+    title: "Baked ricotta cake",
+    included: "Unsalted butter, chilled, chopped, caster sugar, eggs, plain flour, baking powder, fresh ricotta, cream cheese, vanilla extract, Pure icing sugar, to dust",
+    desc: "very delicious",
+    category: "Dessert",
+    price: 12.2,
+    time: 34,
+    serv: 1,
+    calperServ: 23,
+    img: "./images/dessert2.jpg",
+    top: false,
+    type: "breakfast"
+  }, {
+    title: "Pecan baklava rolls",
+    included: "Pecan, cinnamon, nutmeg, fresh filo pastry, sugar, glucose syrup, cinnamon quills, lemon juice",
+    desc: "very delicious",
+    category: "Easy Prep Meals",
+    price: 13.56,
+    time: 34,
+    serv: 1,
+    calperServ: 44,
+    img: "./images/dessert3.jpg",
+    top: false,
+    type: "breakfast"
+  }, {
+    title: "Lemon meringue pie",
+    included: "Shredded coconut, Scottish shortbread biscuits, salted butter, gelatine leaf, egg, milk, lemon juice",
+    desc: "very delicious",
+    category: "Dessert",
+    price: 11.22,
+    time: 34,
+    serv: 1,
+    calperServ: 66,
+    img: "./images/dessert4.jpg",
+    top: true,
+    type: "breakfast"
+  }, {
+    title: "Coconut yoghurt cake",
+    included: "Scottish shortbread biscuits, salted butter, gelatine leaf, egg, milk, lemon juice",
+    desc: "very delicious",
+    category: "Dessert",
+    price: 23.1,
+    time: 34,
+    serv: 1,
+    calperServ: 56,
+    img: "./images/dessert5.jpg",
+    top: false,
+    type: "breakfast"
+  }, {
+    title: "Melt-and-mix white chocolate and ginger mud cake",
+    included: "CADBURY Baking White Chocolate, butter, raising flour, plain flour, ginger, sugar",
+    desc: "very delicious",
+    category: "Dessert",
+    price: 11.45,
+    time: 34,
+    serv: 1,
+    calperServ: 87,
+    img: "./images/dessert6.jpg",
+    top: true,
+    type: "breakfast"
+  },
+  {
+    title: "Tex-Mex Chicken Meal Prep Bowls",
+    included: "Chicken, veggies, insed and drained black beans, rice",
+    desc: "very delicious",
+    category: "Easy Prep Meals",
     price: 30.00,
     time: 22,
     serv: 2,
     calperServ: 750,
-    img: "./images/ingredient1.jpg",
+    img: "./images/easy1.jpg",
     top: false,
     type: "breakfast"
   },
   {
-    title: "Radish",
-    included: "Eggs, green beans, Kalamata olives, tomatoes, tuna, and tangy potatoes",
+    title: "Vegetarian Burrito Bowl with Avocado Crema",
+    included: "Roasted veggies, savory beans and a lime-garlic avocado crema top a bed of cilantro-lime rice in this hearty, healthy vegetarian burrito bowl recipe",
     desc: "very delicious",
-    category: "Clean15 (Low carb)",
-    price: 28.03,
-    time: 23,
+    category: "Vegetarian Meals",
+    price: 11.45,
+    time: 34,
+    serv: 1,
+    calperServ: 87,
+    img: "./images/vegi1.jpg",
+    top: false,
+    type: "breakfast"
+  },
+  {
+    title: "Butternut Squash Soup",
+    included: "Creamy butternut squash soup made with sweet apples, garlic, and thyme is the perfect cozy, light dinner.",
+    desc: "very delicious",
+    category: "Vegetarian Meals",
+    price: 30.00,
+    time: 22,
     serv: 2,
-    calperServ: 920,
-    img: "./images/ingredient2.jpg",
+    calperServ: 750,
+    img: "./images/vegi2.jpg",
     top: false,
     type: "breakfast"
-  },
-  {
-    title: "Pronta",
-    included: "Egg for colour and richness (in some types of pasta), and possibly vegetable juice (such as spinach, beet, tomato, carrot), herbs",
+  }, {
+    title: "Sesame Garlic Ramen Noodles",
+    included: "Ramen noodles, ditch the seasoning packet, and use the noodles to whip up sesame garlic ramen noodles that taste restaurant-worthy but come together in about ten minutes.",
     desc: "very delicious",
-    category: "Clean15 (Low carb)",
+    category: "Vegetarian Meals",
     price: 15.22,
     time: 12,
     serv: 2,
     calperServ: 1000,
-    img: "./images/ingredient3.jpg",
+    img: "./images/vegi3.jpg",
+    top: false,
+    type: "breakfast"
+  }, {
+    title: "Cauliflower Curry",
+    included: "Cumin, Cinnamon, Yellow curry powder, Garam masala, Allspice, Coriander, Cardamom",
+    desc: "very delicious",
+    category: "Vegetarian Meals",
+    price: 11.45,
+    time: 34,
+    serv: 1,
+    calperServ: 87,
+    img: "./images/vegi4.jpg",
     top: false,
     type: "breakfast"
   },
   {
-    title: "bokki",
-    included: "Tteok (rice cakes), fishcake, gochujang",
+    title: "Thai Coconut Red Lentil Soup",
+    included: "1 large chopped onion, Carrots, peeled, Fresh garlic, Thai red curry paste, Coconut milk, Fresh cilantro, Limes",
     desc: "very delicious",
     category: "Family-Style Meals",
-    price: 22.23,
-    time: 8,
-    serv: 3,
-    calperServ: 500,
-    img: "./images/ingredient4.jpg",
-    top: true,
+    price: 15.22,
+    time: 12,
+    serv: 2,
+    calperServ: 1000,
+    img: "./images/family1.jpg",
+    top: false,
     type: "breakfast"
   },
   {
-    title: "Burger",
-    included: "Ground beef, egg, beaten, dry bread crumbs, evaporated milk, Worcestershire sauce, cayenne pepper, garlic, minced",
+    title: "Pappardelle Pasta al Limone",
+    included: "Kosher salt, egg yolk, heavy cream, Parmesan cheese, 1 lemon, pappardelle pasta, butter, salted, chopped parsley",
     desc: "very delicious",
-    category: "Vegetarian Meals",
-    price: 55.2,
+    category: "Family-Style Meals",
+    price: 12.2,
     time: 34,
     serv: 1,
-    calperServ: 940,
-    img: "./images/ingredient5.jpg",
-    top: true,
+    calperServ: 23,
+    img: "./images/family2.jpg",
+    top: false,
+    type: "breakfast"
+  }, {
+    title: "Burst Cherry Tomato Pasta with Ricotta",
+    included: "Kosher salt, cherry tomatoes, preferably mixed colors, extra-virgin olive oil, spaghetti, bucatini, garlic clove, crumbled red chili, Black pepper, whole milk ricotta cheese, parmesan cheese, basil leaves",
+    desc: "very delicious",
+    category: "Family-Style Meals",
+    price: 11.45,
+    time: 34,
+    serv: 1,
+    calperServ: 87,
+    img: "./images/family3.jpg",
+    top: false,
+    type: "breakfast"
+  }, {
+    title: "Gnocchi with Mushrooms and Blue Cheese",
+    included: "Extra-virgin olive oil, white button mushrooms, portobello mushroom caps, Kosher salt, red onion, garlic cloves, tablespoons chopped fresh thyme, white wine, vegetable stock, heavy cream",
+    desc: "very delicious",
+    category: "Family-Style Meals",
+    price: 30.00,
+    time: 22,
+    serv: 2,
+    calperServ: 750,
+    img: "./images/family4.jpg",
+    top: false,
     type: "breakfast"
   },
-
-
 ];
 
 
@@ -155,11 +275,15 @@ module.exports.getTopMeals = function () {
 }
 
 const getMeals = function (cat) {
-  var filtered = [];
+  var filtered = {
+    categoryName: cat,
+    mealkits: []
+  };
+
   for (var i = 0; i < meals.length; i++) {
 
     if (meals[i].category == cat) {
-      filtered.push(meals[i]);
+      filtered.mealkits.push(meals[i]);
     }
 
   }
@@ -170,11 +294,13 @@ const getMeals = function (cat) {
 
 module.exports.getSeperateMeals = function () {
 
-  var filtered = [getMeals("Classic Meals"),
-  getMeals("Clean15 (Low carb)"),
-  getMeals("Easy Prep Meals"),
-  getMeals("Vegetarian Meals"),
-  getMeals("Family-Style Meals")];
+  var filtered = [];
+
+  filtered.push(getMeals("Classic Meals"));
+  filtered.push(getMeals("Dessert"));
+  filtered.push(getMeals("Easy Prep Meals"));
+  filtered.push(getMeals("Vegetarian Meals"));
+  filtered.push(getMeals("Family-Style Meals"));
 
 
   return filtered;
